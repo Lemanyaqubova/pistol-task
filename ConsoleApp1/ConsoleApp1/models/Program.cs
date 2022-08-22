b351d29a50a9aac57748551ff74259b2388a0b61
Lemanyaqubova <lamanny@code.edu.az>
2022-08-22 18:41:16 +0400

    file

---
+++ ConsoleApp1/ConsoleApp1/models/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BulletCapasity");
+            int BulletCapasity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("BulletCapasity");
+           int  BulletCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("DischargeSeconds");
+            float DischargeSeconds = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ShootingMode");
+            string ShootingMode = Console.ReadLine();
+
+            int choice;
+            Weapon gun = new Weapon(BulletCapasity, BulletCount, DischargeSeconds, ShootingMode);
+            do
+
+            {
+                Console.WriteLine("0) get information ");
+                Console.WriteLine("1) Shoot");
+                Console.WriteLine("2)Fire");
+                Console.WriteLine("3)GetRemainBulletCount");
+                Console.WriteLine("4)Reload");
+                Console.WriteLine("5) ChangeFireMode");
+                Console.WriteLine("6)Exit ");
+                Console.WriteLine("7)Edit");
+                 
+                 choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        gun.GetInfo();
+                        break;
+                    case 1:
+                        gun.Shoot();
+                        break;
+                    case 2:
+                        gun.Fire();
+                        break;
+                    case 3:
+                        gun.GetRemainBulletCount();
+                        break;
+                    case 4:
+                        gun.Reload();
+                        break;
+                    case 5:
+                        gun.ChangeFireMode();
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        Console.WriteLine("if you want to change bulletcapacity, presS T ");
+                        Console.WriteLine(" if you want to change bullentcount press S");
+                        Console.WriteLine(" if you want to change dischargesecond press D ");
+                        char a = Convert.ToChar(Console.ReadLine());
+                        Console.WriteLine("New Value: ");
+                        if (a == 'T') gun.BulletCapasity = Convert.ToInt32(Console.ReadLine());
+                        else if (a == 'S') gun.BulletCount = Convert.ToInt32(Console.ReadLine());
+                        else if (a == 'D') gun.DischargeSeconds = Convert.ToSingle(Console.ReadLine());
+                        break;
+
+
+
+                }
+
+            } while (choice != 6);
+            
+        }
+    }
+}
+
---
+++ ConsoleApp1/ConsoleApp1/models/Weapon.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace ConsoleApp1.models
+{
+    class Weapon
+    {
+        private int _bulletCapasity;
+        private int _bulletCount;
+        private float _dischargeSeconds;
+        private string _shootingMode;
+
+
+
+        public int BulletCapasity
+        {
+            get
+            {
+                return _bulletCapasity;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletCapasity = value;
+                }
+            }
+        }
+        public int BulletCount
+        {
+            get
+            {
+                return _bulletCount;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _bulletCount = value;
+                }
+
+
+            }
+
+        }
+        public float DischargeSeconds
+        {
+            get
+            {
+                return _dischargeSeconds;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _dischargeSeconds = value;
+                }
+            }
+        }
+        public string ShootingMode
+        {
+            get
+            {
+                return _shootingMode;
+            }
+            set
+            {
+                if (value == "Single" || value == "Auto")
+                {
+                    _shootingMode = value;
+                }
+            }
+        }
+        public void Shoot()
+        {
+            if (BulletCount > 0)
+
+            {
+                BulletCount--;
+            }
+            Console.WriteLine("shot was fired");
+            if (BulletCount == 0)
+            {
+                Console.WriteLine("the bullet escaped");
+            }
+        }
+        public void Fire()
+        {
+            if (ShootingMode == "automatic") 
+            {
+                
+                float time = (DischargeSeconds / BulletCapasity) * BulletCount;
+                BulletCount = 0;
+                Console.WriteLine(time);
+            }
+           else
+            {
+                BulletCount--;
+            }
+
+        }
+        public int GetRemainBulletCount()
+        {
+            return BulletCapasity - BulletCount;
+        }
+
+        public void Reload()
+        {
+            BulletCount = BulletCapasity;
+            Console.WriteLine("Reload");
+        }
+
+        public void ChangeFireMode()
+        {
+            if (ShootingMode == "Single")
+            {
+                ShootingMode = "Automatic";
+            }
+            else
+            {
+                ShootingMode = "Single";
+            }
+            
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("BulletCapasity:"+ BulletCapasity + "\nBulletCount:"+BulletCount + "\nDischargeSeconds:" + DischargeSeconds +  "\nShootingMode:" +ShootingMode);
+        }
+        public Weapon(int bulletCapasity, int bulletCount, float dischargeSeconds, string shootingMode)
+        {
+            BulletCapasity = bulletCapasity;
+            BulletCount = bulletCount;
+            DischargeSeconds = dischargeSeconds;
+            ShootingMode = shootingMode;
+        }
+       
+          
+
+    }
+
+
+}
+
+    
+
